f8be3b72ee93a4a5b8c6ded20dd571194f9995b8
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-04-21 11:14:43 +0300

    week 7 b4 break

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
-    alert("sa")
+    $("button").click(function (e) { 
+        e.preventDefault();//form nesnesi için kullanılıyor
+        
+        $(this).parent().parent().find(".adi").css("background-color","red");
+        var icerik = $(this).parent().parent().find(".adi").text()
+        mesajGonder(icerik);
 
-    $("h1").click(function (e) {
-        alert("sa")
-        e.preventDefault();
-        $("h1").css("color", "red");
-    })
+        $("button").not(".ekleBtn").click(function (e) { 
+            e.preventDefault();//form nesnesi için kullanılıyor
+            $(this).parent().parent().find(".adi").css("background-color","white");
+        });
+        
+        $(".ekleBtn").click(function (e) {
+            $('table').append('<tr><td class="adi">'+icerik+'</td><td><button class="silBtn">Sil</button></td></tr>');
+        });
+    });
+
+
+    // $(".name2z").hide();
+    // $("h1").click(function (e) {
+    //     e.preventDefault();
+    //     $("h1").css("color", "red");
+    // })
 });
+
+function mesajGonder(deger){
+    alert(deger);
+}
